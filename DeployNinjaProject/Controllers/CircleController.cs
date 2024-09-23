eb550bcd777d91c0c64ea6390b0a7c5032526c08
mohamed-edu <mohamed.mohamud3222@gmail.com>
2024-09-23 15:22:10 +0200

    Add project files.

---
+++ DeployNinjaProject/Controllers/CircleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DeployNinjaProject.Models;
+
+namespace DeployNinjaProject.Controllers
+{
+    public class CircleController : Controller
+    {
+        public ActionResult Index() 
+        { 
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult Calculate(double radius)
+        {
+            var model = new Circle { Radius = radius };
+
+            ViewBag.Circumference = model.GetCircumference();
+
+            ViewBag.Area = model.GetArea();
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ DeployNinjaProject/Models/Circle.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+
+namespace DeployNinjaProject.Models
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+        public double GetCircumference() { return 2 * Math.PI * Radius; }
+
+        public double GetArea() { return Math.PI * Math.Pow(Radius, 2); }
+
+    }
+}
